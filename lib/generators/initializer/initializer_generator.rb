d488229ac53a1d3e4d03b39a6a1a0e5591497263
Jessie Arevalo <dt.jessie7@gmail.com>
2020-01-29 02:23:05 +0800

    Update version fixed key and initializer generator

--- lib/generators/initializer/initializer_generator.rb
+++ lib/generators/initializer/initializer_generator.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class InitializerGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../../templates', __dir__)
+  source_root File.expand_path('templates', __dir__)
   desc 'Create Lalamove initializer for your application'
 
   def copy_initializer_file
